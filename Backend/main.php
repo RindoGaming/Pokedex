eb6543cfabfc4d1183213222e69510e97a236237
Caspar Laponder <Casparlaponderjo@gmail.com>
2025-08-26 11:17:44 +0200

    datainserttest

--- Backend/main.php
+++ Backend/main.php
@@ -1,29 +1,27 @@
 <?php
 
-use Kreait\Firebase\Factory;
 
-$factory = (new $factory)
-    ->withServiceAccount('/path/to/firebase_credentials.json')
-    ->withDatabaseUri('https://my-project-default-rtdb.firebaseio.com');
 
-$auth = $factory->createAuth();
-$realtimeDatabase = $factory->createDatabase();
-$cloudMessaging = $factory->createMessaging();
-$remoteConfig = $factory->createRemoteConfig();
-$cloudStorage = $factory->createStorage();
-$firestore = $factory->createFirestore();
 
-$firebase_url = 'https://pokemondata-565f5-default-rtdb.europe-west1.firebasedatabase.app/';
+$firebase_url = 'https://pokemondata-565f5-default-rtdb.europe-west1.firebasedatabase.app/pokemon.json';
+
+$new_pokemon = [
+    'name' => 'Pikachu',
+    'type' => 'Electric'
+];
+
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $firebase_url);
+curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($new_pokemon));
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+
 
 $response = curl_exec($ch);
 curl_close($ch);
 
-$data = json_decode($response, true);
-
-print_r($data);
+echo $response;
 
 ?>
